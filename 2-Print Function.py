7c932a47bd70bf4764de5be450d91e8295a71339
rakeshjhadbg <64641294+rakeshjhadbg@users.noreply.github.com>
2022-02-02 12:55:33 +0530

    Add files via upload
    Print Function Sample code

---
+++ 2-Print Function.py
@@ -0,0 +1,62 @@
+print("Hello, World!")
+
+
+print("The itsy bitsy spider climbed up the waterspout.")
+print("Down came the rain and washed the spider out.")
+
+
+
+print("The itsy bitsy spider climbed up the waterspout.")
+print()
+print("Down came the rain and washed the spider out.")
+
+
+
+print("The itsy bitsy spider\nclimbed up the waterspout.")
+print()
+print("Down came the rain\nand washed the spider out.")
+
+
+
+print("The itsy bitsy spider\nclimbed up the waterspout.")
+print()
+print("Down came the rain\nand washed the spider out.")
+
+
+print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
+
+
+print("My name is", "Python.")
+print("Monty Python.")
+
+
+
+print("My name is", "Python.", end=" ")
+print("Monty Python.")
+
+
+
+print("My name is ", end="")
+print("Monty Python.")
+
+
+print("My", "name", "is", "Monty", "Python.", sep="-")
+
+
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+print("Programming","Essentials","in")
+print("Python")
+
+
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+
